51bf3b7acc9d162d23481d690b3c5d088d62f33c
v2bejarry <v2bejarry@hotmail.com>
2025-10-16 16:46:47 +0200

    api ok

--- blackjack/api.py
+++ blackjack/api.py
@@ -1,12 +1,27 @@
-from ninja import NinjaAPI, Shema
+from ninja import NinjaAPI
+from ninja import Schema
 
 api = NinjaAPI()
 
-@api.get("/add")
-def add(request, a: int, b: int):
-    return {"result": a + b}
-     
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", api.urls),
-]
+class Player(Schema):
+    player_name : str
+    score : int
+    skiped : bool
+    isWinner: bool
+
+
+class Game(Schema):
+    id : int
+    name : str
+    turn : int
+    gameAsEnded : bool
+
+@api.post("/players", response=Player)
+def create_player(request, data: Player):
+    player = Player.objects.create(**data.dict())
+    return player
+
+@api.post("/games", response=Game)
+def create_game(request, data: Game):
+    game = Game.objects.create(**data.dict())
+    return game
--- blackjack/models.py
+++ blackjack/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Player(models.Model):
     player_name = models.CharField(max_length=200)
-    text_field = models.TextField()
     score = models.IntegerField()
+    isWinner = models.BooleanField(default = False)
     skiped = models.BooleanField(default = False) #à arrété de jouer
 
 class Game(models.Model):
     name = models.CharField(max_length=200, default = 'name')
     turn = models.IntegerField()
-    gameEnd = models.BooleanField(default = False)
+    gameAsEnded = models.BooleanField(default = False)
